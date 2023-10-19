343af304b8f63a8e77753a68df5c9c67459e606e
gxnas <52808122+gxnas@users.noreply.github.com>
2023-10-19 22:40:24 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -4,12 +4,6 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
-#添加chatgpt插件
-git clone https://github.com/sirpdboy/chatgpt-web.git package/luci-app-chatgpt
-
-#添加partexp插件
-git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
-
 #添加CPU使用率、编译作者、固件下载地址
 sed -i '/<tr><td width="33%"><%:CPU usage/a <tr><td width="33%"><%:Compiler author%></td><td><a target="_blank" href="https://wp.gxnas.com">【GXNAS博客】https://wp.gxnas.com</a></td></tr>' package/lean/autocore/files/x86/index.htm
 sed -i '5a\msgid "Compiler author"' feeds/luci/modules/luci-base/po/zh-cn/base.po
@@ -21,7 +15,6 @@ sed -i '/<tr><td width="33%"><%:Compiler author/a <tr><td width="33%"><%:Firmwar
 sed -i '8a\msgid "Firmware Update"' feeds/luci/modules/luci-base/po/zh-cn/base.po
 sed -i '9a\msgstr "固件出处"' feeds/luci/modules/luci-base/po/zh-cn/base.po
 sed -i '10a \\' feeds/luci/modules/luci-base/po/zh-cn/base.po
-
 
 # 后台IP设置
 export Ipv4_ipaddr="192.168.1.1"             # 修改openwrt后台地址(填0为关闭)
