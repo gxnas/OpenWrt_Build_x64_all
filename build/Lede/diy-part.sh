86e3792e7dec15d6584efa86ed5b824cf68c246f
gxnas <52808122+gxnas@users.noreply.github.com>
2024-06-12 22:38:25 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -7,10 +7,6 @@
 #添加homeproxy插件
 git clone https://github.com/douglarek/luci-app-homeproxy.git package/luci-app-homeproxy
 
-#添加bypass插件
-git clone -b bypass https://github.com/shidahuilang/openwrt-package.git package/luci-app-bypass
-
-
 #添加CPU使用率、编译作者、固件下载地址
 sed -i '/<tr><td width="33%"><%:CPU usage/a <tr><td width="33%"><%:Compiler author%></td><td><a target="_blank" href="https://wp.gxnas.com">【GXNAS博客】https://wp.gxnas.com</a></td></tr>' package/lean/autocore/files/x86/index.htm
 sed -i '5a\msgid "Compiler author"' feeds/luci/modules/luci-base/po/zh-cn/base.po
